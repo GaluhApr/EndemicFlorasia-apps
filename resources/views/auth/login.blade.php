d0341933378534a3eaf87bed5fc0b7f2b51f8796
Gusde Biyan Ambarista <gusdebiyan28@gmail.com>
2023-06-17 22:50:24 +0700

    edit color

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,14 +11,12 @@
     <meta name="generator" content="Hugo 0.112.5">
     <title> EndemicFlorasia | Login</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
 
     <link href="/assets/css/login.css" rel="stylesheet">
     <!-- CSS Files -->
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
     <link href="/assets/css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet" />
 
-    <link href="/sweetalert2/sweetalert2.min.css" rel="stylesheet" />
 </head>
 
 <body class="d-flex align-items-center py-4 " background="/assets/img/hero-bg.jpg">
@@ -28,8 +26,9 @@
             @csrf
 
             <div class="card">
-                <h1 class="h3 mb-3 fw-normal">Welcome Admin</h1>
-                <div class="form-group">
+                <img src="/frontend/assets/img/logo2.png" class="logo-" />
+
+                <div class="form-group pt-5">
                     <label for="floatingInput">Email</label>
                     <input type="email" id="email"
                         class="form-control  bg-light @error('email') is-invalid @enderror" name="email"
@@ -52,8 +51,6 @@
             </div>
         </form>
     </main>
-    <script src="/assets/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script>
 
 </body>
 
